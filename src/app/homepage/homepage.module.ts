6325b3cc6bd2e280f2ad3dd6cccf84c3c0680efe
BuildTools <m.sagiroglu@outlook.de>
2022-09-05 22:05:10 +0200

    set up homepage page picture and description

--- src/app/homepage/homepage.module.ts
+++ src/app/homepage/homepage.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomepageComponent } from './homepage/homepage.component';
+import {RouterModule} from '@angular/router';
 
 
 
@@ -8,8 +9,9 @@ import { HomepageComponent } from './homepage/homepage.component';
   declarations: [
     HomepageComponent
   ],
-  imports: [
-    CommonModule
-  ]
+    imports: [
+        CommonModule,
+        RouterModule
+    ]
 })
 export class HomepageModule { }
